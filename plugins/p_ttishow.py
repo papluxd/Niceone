3ccfbfdf44673627e46059d20f647f9355055858
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-08-31 15:48:57 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/teammoviesupdates')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/DTG_BOTS')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
