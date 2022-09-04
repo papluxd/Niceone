91c970e37cc7dfbf6c89b67b45da5e4ea4f26135
COmboCong <111708072+COmboCong@users.noreply.github.com>
2022-09-04 23:49:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -376,7 +376,7 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-shortz = shortzy.Shortzy(SHORTENER_API, "droplink.co")
+shortz = shortzy.Shortzy(SHORTENER_API, "mdiskshortner.in")
 async def get_shortlink(link):
     if SHORTENER_API:
         if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
