27728f1013e0fdcfb1e27c830b3daa8dca225659
COmboCong <111708072+COmboCong@users.noreply.github.com>
2022-09-06 14:13:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -102,7 +102,7 @@ Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
 """
-    LOG_TEXT_P = """#NewUser
+    LOG_TEXT_P = """#NewUserforTeamTb
 ID - <code>{}</code>
 Name - {}
 """
